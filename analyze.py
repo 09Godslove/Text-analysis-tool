c7f0634322fc67f770fa9841a00a77620f4b4ea0
09Godslove <200godslove@gmail.com>
2025-10-20 20:32:12 +0100

    getting username

---
+++ analyze.py
@@ -0,0 +1,9 @@
+print('\nWelcome to the text analysis tool, I will mine and analyze a body of text you send me')
+
+#get username
+
+username= input('\nTo begin please, Enter your name: \n')
+
+#greet the users
+
+print('Welcome, ' + username)
