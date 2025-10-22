589c8ad21f50be168f19794e193d9a485c83040a
09Godslove <200godslove@gmail.com>
2025-10-22 13:59:57 +0100

    added functions

--- analyze.py
+++ analyze.py
@@ -1,9 +1,15 @@
-print('\nWelcome to the text analysis tool, I will mine and analyze a body of text you send me')
+def welcomeUser():
+    print('\nWelcome to the text analysis tool, I will mine and analyze a body of text you send me')
 
 #get username
+def getUsername():
+    userNameFromInput =input('\nTo begin please, Enter your name: \n')
+    return userNameFromInput
 
-username= input('\nTo begin please, Enter your name: \n')
+username= getUsername()
 
 #greet the users
+def greetUser(name):
+    print('Welcome, ' + name)
 
-print('Welcome, ' + username)
+greetUser(username)
