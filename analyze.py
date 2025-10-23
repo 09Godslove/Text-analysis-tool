3d669e9ddb05022c6a4802127284f0d9ebc52db4
09Godslove <200godslove@gmail.com>
2025-10-23 14:09:35 +0100

    Validated Username

--- analyze.py
+++ analyze.py
@@ -1,9 +1,18 @@
+from random_username.generate import generate_username
+
 def welcomeUser():
     print('\nWelcome to the text analysis tool, I will mine and analyze a body of text you send me')
 
 #get username
 def getUsername():
     userNameFromInput =input('\nTo begin please, Enter your name: \n')
+
+    if  (len(userNameFromInput) < 5 or not userNameFromInput.isidentifier()):
+        print('Your username has to be greater than 5 character, alphanumeric and not contain spaces')
+        userNameFromInput = generate_username()[0]
+        print('Assigning a default Username....')
+
+
     return userNameFromInput
 
 username= getUsername()
