27140c05f4c9d24c18f54709c8049323672db41d
09Godslove <200godslove@gmail.com>
2025-12-04 15:40:59 +0100

    got basic stock info

--- stockAnalyzer.py
+++ stockAnalyzer.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+from datetime import datetime
 
 def etractbasicInfo (data):
     keysToExtract = ['longName','website','sector','fullTimeEmployees', 'marketCap', 'totalCash', 'trailingEps' ]
@@ -11,12 +12,42 @@ def etractbasicInfo (data):
     return basicInfo
 
     
+def getStockhistory(company):
+    hst = company.history(period='12mo')
+    prices = hst["Open"].tolist()
+    dates = hst.index.strftime('%Y-%m-%d').tolist()
+    return {
+        'Price': prices,
+        'Date': dates
+    }
 
+def getEarningDates(company):
+    earningDatedf = company.earnings_dates
+    allDates = earningDatedf.index.strftime('%Y-%m-%d').tolist()
+    dateObjects = [datetime.strptime(date, '%Y-%m-%d') for date in allDates]
+    currentDate = datetime.now()
+    futureDates = [date.strftime('%Y-%m-%d') for date in dateObjects if date >currentDate]
+    return futureDates
+def getNews(company):
+    newsList = company.news
+    allNewsArticles = []
+    for newsdict in newsList:
+        content = newsdict.get("content", {})
 
+        newsDictToAdd = {
+            "title": content.get("title"),
+            "link": content.get("canonicalUrl", {}).get("url")
+        }
+        allNewsArticles.append(newsDictToAdd)
+    return allNewsArticles
 def getCompanystockInfo (TickerSymbol):
-
+    # get data from yahoo finance
     company = yf.Ticker(TickerSymbol)
-    basicinfo = etractbasicInfo(company.info)
-    print(basicinfo)
+
+    # collect all basic info
+    basicinfo = etractbasicInfo(company.info) 
+    PriceHistory = getStockhistory(company)
+    futureEarningDates = getEarningDates(company)
+    newsArticles = getNews(company)
 
 getCompanystockInfo('MSFT')
