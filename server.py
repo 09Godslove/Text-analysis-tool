afe7480648beaea0a0f22cb5770fc38d789123c5
09Godslove <200godslove@gmail.com>
2025-12-16 10:44:56 +0100

    Added basic flask server

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/health')
+def helloWorld():
+    return 'Flask server is running'
+
+
+if __name__ == '__main__':
+
+
+    app.run()
+
