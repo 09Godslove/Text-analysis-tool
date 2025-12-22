e573e079cd71fafee110152fb400e25eb8cf4f91
09Godslove <200godslove@gmail.com>
2025-12-22 20:18:24 +0100

    Allowed CORS

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, abort, request
 from stockAnalyzer import getCompanystockInfo
 from analyze import analyzeAticle
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 
 @app.route('/health', methods= ['GET'])
